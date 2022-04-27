a883383b0a22bb53ffcf6c5dd04ff7a8dba883d2
Eder Basso <eder.sdz@gmail.com>
2022-04-27 19:29:23 -0300

    first commit

---
+++ src/com/letscode/Aula2704.java
@@ -0,0 +1,21 @@
+package com.letscode;
+
+public class Aula2704 {
+
+    private String nome; //null
+    public int idade; //0
+    boolean vivo; //false
+
+    public void metodo1(){
+
+    }
+
+    public void metodo2(){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+
+}
